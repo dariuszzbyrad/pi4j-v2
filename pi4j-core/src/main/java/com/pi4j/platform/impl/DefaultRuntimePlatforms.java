7dc929ac3438f04099f420414e90a98d84dc9db9
Frank Delporte <fdelporte@azul.com>
2024-11-16 14:55:33 +0100

    Use List.of

--- pi4j-core/src/main/java/com/pi4j/platform/impl/DefaultRuntimePlatforms.java
+++ pi4j-core/src/main/java/com/pi4j/platform/impl/DefaultRuntimePlatforms.java
@@ -37,10 +37,7 @@ import com.pi4j.runtime.Runtime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
