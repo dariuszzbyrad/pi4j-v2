e62ad6398fb6aef7c8890d8a49fdde17bca9ca16
Frank Delporte <frank@webtechie.be>
2024-11-20 08:08:15 +0100

    Remove flaky test of detectedBoard.getBoardModel().getModel()

--- pi4j-core/src/test/java/com/pi4j/boardinfo/util/BoardModelDetectionTest.java
+++ pi4j-core/src/test/java/com/pi4j/boardinfo/util/BoardModelDetectionTest.java
@@ -1,6 +1,5 @@
 package com.pi4j.boardinfo.util;
 
-import com.pi4j.boardinfo.definition.PiModel;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -13,16 +12,17 @@ class BoardModelDetectionTest {
         var detectedBoard = BoardInfoHelper.current();
 
         assertAll(
-                () -> assertEquals(detectedBoard.getOperatingSystem().getName(), System.getProperty("os.name")),
-                () -> assertEquals(detectedBoard.getOperatingSystem().getVersion(), System.getProperty("os.version")),
-                () -> assertEquals(detectedBoard.getOperatingSystem().getArchitecture(), System.getProperty("os.arch")),
+            () -> assertEquals(detectedBoard.getOperatingSystem().getName(), System.getProperty("os.name")),
+            () -> assertEquals(detectedBoard.getOperatingSystem().getVersion(), System.getProperty("os.version")),
+            () -> assertEquals(detectedBoard.getOperatingSystem().getArchitecture(), System.getProperty("os.arch")),
 
-                () -> assertEquals(detectedBoard.getJavaInfo().getVersion(), System.getProperty("java.version")),
-                () -> assertEquals(detectedBoard.getJavaInfo().getRuntime(), System.getProperty("java.runtime.version")),
-                () -> assertEquals(detectedBoard.getJavaInfo().getVendor(), System.getProperty("java.vendor")),
-                () -> assertEquals(detectedBoard.getJavaInfo().getVendorVersion(), System.getProperty("java.vendor.version")),
+            () -> assertEquals(detectedBoard.getJavaInfo().getVersion(), System.getProperty("java.version")),
+            () -> assertEquals(detectedBoard.getJavaInfo().getRuntime(), System.getProperty("java.runtime.version")),
+            () -> assertEquals(detectedBoard.getJavaInfo().getVendor(), System.getProperty("java.vendor")),
+            () -> assertEquals(detectedBoard.getJavaInfo().getVendorVersion(), System.getProperty("java.vendor.version"))
 
-                () -> assertEquals(detectedBoard.getBoardModel().getModel(), PiModel.UNKNOWN) // Only valid on PC, macOS or build server
+            // Cannot test detectedBoard.getBoardModel().getModel()) as the output is different on
+            // Raspberry Pi versus PC, macOS or build server
         );
     }
 }
