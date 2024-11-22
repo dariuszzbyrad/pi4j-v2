b213ecca9559f75d915c2105e981b3673c5ac2fa
Frank Delporte <frank@webtechie.be>
2024-11-22 13:11:35 +0100

    Extended use of List.of instead of Collections

--- pi4j-core/src/main/java/com/pi4j/boardinfo/definition/HeaderVersion.java
+++ pi4j-core/src/main/java/com/pi4j/boardinfo/definition/HeaderVersion.java
@@ -1,14 +1,13 @@
 package com.pi4j.boardinfo.definition;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public enum HeaderVersion {
     PICO("Pico", "Used on the Pico microcontroller", new ArrayList<>()),
-    TYPE_1("Type 1", "Used on original Model B", Collections.singletonList(HeaderPins.HEADER_26_TYPE_1)),
+    TYPE_1("Type 1", "Used on original Model B", List.of(HeaderPins.HEADER_26_TYPE_1)),
     TYPE_2("Type 2", "Used on Model A and Model B (revision 2)", List.of(HeaderPins.HEADER_26_TYPE_2, HeaderPins.HEADER_8)),
-    TYPE_3("Type 3", "Used on Model A+, B+, Pi Zero, Pi Zero W, Pi2B, Pi3B, Pi4B, Pi5B", Collections.singletonList(HeaderPins.HEADER_40)),
+    TYPE_3("Type 3", "Used on Model A+, B+, Pi Zero, Pi Zero W, Pi2B, Pi3B, Pi4B, Pi5B", List.of(HeaderPins.HEADER_40)),
     COMPUTE("Compute Module", "54 GPIO", List.of(HeaderPins.COMPUTE_J5, HeaderPins.COMPUTE_J6)),
     UNKNOWN("Unknown", "", new ArrayList<>());
 
